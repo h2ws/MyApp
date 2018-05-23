248e1a3d7d3a1489d63f67f64a3d6db51347a33a
oy_y <2846881204@qq.com>
2018-05-23 09:41:45 +0800

    新增sayHello

--- MyApp/src/Hello.java
+++ MyApp/src/Hello.java
@@ -3,4 +3,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello git, this is my javapoject");
 	}
+	
+	static void sayHello() {
+		System.out.println("Hello");
+	}
 }
