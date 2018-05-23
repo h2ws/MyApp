7a3de1aee3bb8c7c5b8550917d1b6e657082caf6
oy_y <2846881204@qq.com>
2018-05-23 09:46:09 +0800

    sayHello方法加强，新增name参数

--- MyApp/src/Hello.java
+++ MyApp/src/Hello.java
@@ -3,11 +3,11 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello git, this is my javapoject");
 		
-		sayHello();
-		sayHello();
+		sayHello("Bob");
+		sayHello("Jack");
 	}
 	
-	static void sayHello() {
-		System.out.println("Hello");
+	static void sayHello(String name) {
+		System.out.println("Hello my name is "+name);
 	}
 }
